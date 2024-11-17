9e8b9cbd5ced1c3cf7cd9a2ac008aa6932dfa66b
VioletPR3080 <3080499763@qq.com>
2024-11-17 19:33:31 +0800

    显示版本号，具体提示

--- GPLTT.py
+++ GPLTT.py
@@ -11,6 +11,7 @@ import json
 from collections import deque
 print("space.bilibili.com/167480068")
 print("repo: github.com/VioletPR3080/GPLTT")
+print("version: 0.3.1")
 
 def refresh_options(prompt):
     match prompt:
